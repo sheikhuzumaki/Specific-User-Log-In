af1fa798710e83725a98740d51823abe6240a0e9
sheikhuzumaki <65375513+sheikhuzumaki@users.noreply.github.com>
2020-06-13 05:14:18 +0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,43 @@
+<?php
+
+  session_start();
+  
+  $conn = mysqli_connect('localhost', 'root', '', 'emptyblog');
+
+  $info = $_SESSION['email'] ;
+
+  $query = "SELECT * FROM practise WHERE email = '$info'";
+  $result = mysqli_query($conn , $query);
+  $output = mysqli_fetch_assoc($result);
+
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Home</title>
+  </head>
+  <body>
+    
+    <nav class="navbar bg-primary text-white">
+      <div class="container">
+        <h1>Welcome <?php echo $output['fname']; ?></h1>
+        <form method="GET" action="login.php">
+          <input type="submit" name="back" class="btn btn-success" value="Back to Log In">
+        </form>
+      </div>
+    </nav>
+
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
